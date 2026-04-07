0146772a5c1a2bc5c2f521d021b86bf1e635b940
Xin Li <59580070+xin-hedera@users.noreply.github.com>
2026-04-07 10:19:08 -0500

    Bump minimum HAPI version for hooks to 0.73.0 (#13295)
    - Bump minimum HAPI version for hooks to 0.73.0
    
    Signed-off-by: Xin Li <xin@hashgraph.com>

--- test/src/test/java/org/hiero/mirror/test/e2e/acceptance/config/FeatureProperties.java
+++ test/src/test/java/org/hiero/mirror/test/e2e/acceptance/config/FeatureProperties.java
@@ -19,7 +19,7 @@ public class FeatureProperties {
 
     private boolean contractCallLocalEstimate = true;
 
-    private int hapiMinorVersionWithHooks = 72;
+    private int hapiMinorVersionWithHooks = 73;
 
     private int hapiMinorVersionWithoutGasRefund = 69;
 
