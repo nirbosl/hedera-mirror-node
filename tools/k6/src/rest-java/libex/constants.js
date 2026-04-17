29b7428e08dbf937ff844121b3fa92ba16c8a903
Stoyan Dimitrov <stoyan.dimitrov@limechain.tech>
2026-04-17 16:54:58 +0300

    Fix k6 tests constant name (#13365)
    - Fix k6 tests constant name
    
    Signed-off-by: sdimitrov9 <stoyan.dimitrov@limechain.tech>

--- tools/k6/src/rest-java/libex/constants.js
+++ tools/k6/src/rest-java/libex/constants.js
@@ -5,4 +5,4 @@ export const airdrops = 'airdrops';
 export const hooks = 'hooks';
 export const hookStorage = 'storage';
 export const nodeListName = 'nodes';
-export const registeredNodes = 'registered-nodes';
+export const registeredNodes = 'registered_nodes';
