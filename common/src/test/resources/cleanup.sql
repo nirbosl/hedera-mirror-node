63f433becbe4b5bb214d9313ba190b3d8eb3d5da
Stoyan Dimitrov <stoyan.dimitrov@limechain.tech>
2026-09-18 18:57:22 +0300

    Render table names as quoted identifiers in cleanup SQL scripts (#14304)
    Use the %I (identifier) format specifier instead of %s in the dynamic delete / lock table / truncate statements of the cleanup SQL scripts.
    
    Signed-off-by: sdimitrov9 <stoyan.dimitrov@limechain.tech>

--- common/src/test/resources/cleanup.sql
+++ common/src/test/resources/cleanup.sql
@@ -9,7 +9,7 @@ for t in select table_name
 from information_schema.tables
 where table_schema = ''public'' and table_name !~ ''.*(flyway|transaction_type|citus_|_\d+).*'' and table_type <> ''VIEW''
     loop
-    execute format(''delete from %s'', t.table_name);
+    execute format(''delete from %I'', t.table_name);
     end loop;
 end;
 ';
--- importer/src/main/resources/db/scripts/cleanup.sql
+++ importer/src/main/resources/db/scripts/cleanup.sql
@@ -9,8 +9,8 @@ for t in select table_name
 from information_schema.tables
 where table_schema = ''public'' and table_name !~ ''.*(flyway|transaction_type|citus_|_\d+).*'' and table_type <> ''VIEW''
     loop
-    execute format(''lock table %s in access exclusive mode'', t.table_name);
-    execute format(''truncate %s restart identity cascade'', t.table_name);
+    execute format(''lock table %I in access exclusive mode'', t.table_name);
+    execute format(''truncate %I restart identity cascade'', t.table_name);
     commit;
     end loop;
 end;
